807b787888c5590983d79b79b4a88bff04c79e1d
AshishRanjan <aviashishranjan@gmail.com>
2022-11-18 14:07:18 +0530

    Adding ImageView in Map Section

--- src/features/map/component/map-callout.component.js
+++ src/features/map/component/map-callout.component.js
@@ -1,8 +1,7 @@
 import React from "react";
-import styled from "styled-components";
 
-const MyText = styled.Text``;
+import { CompactRestaurantInfo } from "../../../components/restaurant/Compact-restaurant-info.component";
 
 export const MapCallout = ({ restaurant }) => (
-  <MyText>{restaurant.name}</MyText>
+  <CompactRestaurantInfo restaurant={restaurant} />
 );
